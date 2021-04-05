bc84372ff84f31f06606e4c5ec89e98b7ca3a951
Carlos Une <brocolis@eml.cc>
2021-04-04 22:29:02 -0300

    Update test1 example

--- test/test1.f90
+++ test/test1.f90
@@ -1,12 +1,13 @@
 program test1
 use cairo
+use cairo_enums
 use iso_c_binding, only: c_ptr, c_int, c_null_char
 implicit none
 type(c_ptr) :: surface, c
 integer(c_int) :: r
 
 ! Initialize
-surface = cairo_image_surface_create(0, 200, 200)
+surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 200, 200)
 c = cairo_create(surface)
 
 ! Set background colour
@@ -17,7 +18,7 @@ call cairo_fill(c)
 
 ! Draw 'F'
 call cairo_set_source_rgb(c, 1.d0, 1.d0, 1.d0)
-call cairo_select_font_face(c, "Clarendon BT Roman"//c_null_char, 0, 0)
+call cairo_select_font_face(c, "Clarendon BT Roman"//c_null_char, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
 call cairo_set_font_size(c, 72.0d0)
 call cairo_move_to(c, 40d0, 80d0)
 call cairo_show_text(c, "F"//c_null_char)
